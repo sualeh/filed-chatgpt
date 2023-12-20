6276de8be24848ede2c7cd107cae114e5e01eb11
Sualeh Fatehi <sualeh@hotmail.com>
2023-12-20 13:47:02 -0500

    Fix tests

--- tests/test_filed_chatgpt.py
+++ tests/test_filed_chatgpt.py
@@ -10,16 +10,15 @@ from filed_chatgpt.filed_chatgpt import main
 
 class TestFileChatGPT(unittest.TestCase):
 
-    def capture_stdout(self, *args):
+    def __capture_stdout(self, *args):
         with patch.object(sys, 'argv', ['filed_chatgpt.py'] + list(args)):
             with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
                 main()
                 return mock_stdout.getvalue().strip()
 
-
     def test_main_with_input(self):
-        out = self.capture_stdout('-o', 'file', '-k', 'apikey123')
+        out = self.__capture_stdout('-o', 'file', '-k', 'apikey123')
         self.assertEqual(
             out,
-            "Namespace(model='gpt-3.5-turbo', api_key='apikey123', output_file='file')"
+            'Using model: gpt-3.5-turbo and output file: file'
         )
